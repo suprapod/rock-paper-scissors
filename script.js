25f423d5269d9c76faf5c23a1b45862006aa3929
suprapod <176808482+suprapod@users.noreply.github.com>
2024-12-18 17:27:30 +0100

    creationf of the getHumanChoice function

--- script.js
+++ script.js
@@ -15,3 +15,24 @@ function getComputerChoice() {
         return "scissors";
     }
 }
+
+// getHumanChoice will return one of the valid choices depending on what the user inputs.
+
+function getHumanChoice() {
+
+    let humanChoice = prompt("What's your play ? Choose between Rock, Paper and Scissors.")
+
+    if (humanChoice === "rock") {
+        console.log("You choosed to play Rock...")
+        return humanChoice;
+    } else if (humanChoice === "paper") {
+        console.log("You choosed to play Paper...")
+        return humanChoice;
+    } else if (humanChoice === "scissors") {
+        console.log("You choosed to play Scissors...")
+        return humanChoice;
+    } else {
+        console.log("Incorrect syntax, please write it properly.")
+        return getComputerChoice();
+    }
+}
